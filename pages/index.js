3c6e786ad66b74bc787f9a259dcede3708d7d785
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-10-29 18:05:20 +0800

    initial contents

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+        <main className={styles.main}>
+          <div className="landing">
+            <h2>Inks and Watercolor</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              pulvinar quam justo, sed euismod nibh condimentum non. Donec at
+              ante ut turpis consectetur gravida ut eget odio. Aenean efficitur
+              vulputate odio, ac elementum nisi condimentum at. Phasellus vitae
+              justo vitae turpis mollis maximus sed ut ante.
             </p>
-          </a>
-        </div>
-      </main>
+          </div>
+        </main>
+      </div>
+
+      <div className="aboutSection">
+        Matsutani has long combined two and three dimensinal works on canvas,
+        paper and in installations.
+      </div>
+
+      <div className="gallerySection">
+        GALLERY
+      </div>
+
+      <div className="contactSection">
+        <button>CONTACT ME</button>
+      </div>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>John Doe</p>
+        <p>janedoe@email.com</p>
+        <p>Site by: Arvin Mark Atienza</p>
+        <p>&copy; 2020</p>
       </footer>
-    </div>
-  )
+    </>
+  );
 }
