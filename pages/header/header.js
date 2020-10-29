fbf5b651bbce02613060b0a368fdf4c098f5cf8d
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-10-29 18:55:17 +0800

    landing page

---
+++ pages/header/header.js
@@ -0,0 +1,51 @@
+// Dependencies
+import React, { useEffect } from "react";
+import Fade from "react-reveal/Fade";
+// Assets
+import styles from "./header.module.css";
+
+export default function Header() {
+  const [visible, setVisibility] = React.useState(true);
+  const [prevScrollpos, setPrevScrollpos] = React.useState(
+    typeof window !== "undefined" ? window.pageYOffset : ""
+  );
+
+  const handleScroll = () => {
+    const prevScrollPosition = prevScrollpos;
+
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollPosition >= currentScrollPos;
+
+    setPrevScrollpos(currentScrollPos);
+    setVisibility(visible);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  return (
+    <div className={styles.headerWrapper}>
+      <div className={`${visible ? styles.header : styles.scrolled}`}>
+        <Fade cascade top delay={4200} duration={1300}>
+          <img src="/logo.png" className={styles.logo} />
+          <ul className={styles.list}>
+            <a href="#about">
+              <li className={styles.listItem}>about.</li>
+            </a>
+            <a href="#work">
+              <li className={styles.listItem}>work.</li>
+            </a>
+            <a href="#projects">
+              <li className={styles.listItem}>projects.</li>
+            </a>
+            <a href="#contact">
+              <li className={styles.listItem}>contact.</li>
+            </a>
+          </ul>
+        </Fade>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,52 @@
 import Head from "next/head";
+import Header from "./header/header";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.container}>
+      <div className={styles.bgContainer}>
+        <img src="/overlay.jpeg" className={styles.bgOverlay} />
+      </div>
+      <Header />
+      <div className={styles.mainContainer}>
         <Head>
           <title>Create Next App</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main className={styles.main}>
-          <div className="landing">
-            <h2>Inks and Watercolor</h2>
-            <p>
+          <div className={styles.landing}>
+            <p className={styles.landingTitle}>Inks & Watercolor</p>
+          </div>
+          <div className={styles.descriptionContainer}>
+            <p className={styles.landingDescription}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
               pulvinar quam justo, sed euismod nibh condimentum non. Donec at
               ante ut turpis consectetur gravida ut eget odio. Aenean efficitur
-              vulputate odio, ac elementum nisi condimentum at. Phasellus vitae
-              justo vitae turpis mollis maximus sed ut ante.
+              vulputate odio, ac elementum nisi condimentum at.
             </p>
           </div>
         </main>
-      </div>
 
-      <div className="aboutSection">
-        Matsutani has long combined two and three dimensinal works on canvas,
-        paper and in installations.
-      </div>
+        <div className="aboutSection">
+          Matsutani has long combined two and three dimensinal works on canvas,
+          paper and in installations.
+        </div>
 
-      <div className="gallerySection">
-        GALLERY
-      </div>
+        <div className="gallerySection">GALLERY</div>
 
-      <div className="contactSection">
-        <button>CONTACT ME</button>
-      </div>
+        <div className="contactSection">
+          <button>CONTACT ME</button>
+        </div>
 
-      <footer className={styles.footer}>
-        <p>John Doe</p>
-        <p>janedoe@email.com</p>
-        <p>Site by: Arvin Mark Atienza</p>
-        <p>&copy; 2020</p>
-      </footer>
+        <footer className={styles.footer}>
+          <p>John Doe</p>
+          <p>janedoe@email.com</p>
+          <p>Site by: Arvin Mark Atienza</p>
+          <p>&copy; 2020</p>
+        </footer>
+      </div>
     </>
   );
 }
