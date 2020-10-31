599ed5b7a92560f96b1f7f816ca9e1b9b86ece72
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-10-31 14:45:20 +0800

    updated full page loader

--- pages/index.js
+++ pages/index.js
@@ -19,95 +19,97 @@ export default function Home() {
   const [photoIndex, setPhotoIndex] = useState(0);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.bgContainer}>
-        <img src="/overlay.jpeg" className={styles.bgOverlay} />
-        <div className={styles.bgContainer2}>
-          <img src="/overlay_2.jpeg" className={styles.bgOverlay2} />
-        </div>
-      </div>
-      <ReactBnbGallery
-        show={isOpen}
-        photos={PHOTOS}
-        activePhotoIndex={photoIndex}
-        onClose={() => setIsOpen(false)}
-      />
-      <Header />
-      <div className={styles.mainContainer}>
-        <Head>
-          <title>Inks and Watercolor</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <main className={styles.main}>
-          <div className={styles.landing}>
-            <p className={styles.landingTitle}>Inks & Watercolor</p>
-          </div>
-          <div className={styles.descriptionContainer}>
-            <p className={styles.landingDescription}>
-              Inks and Watercolors is an incubator of modern art based in
-              Iloilo, Philippines. Stretching out of the comfort zone to pursue
-              painting and illustration in any medium.
-            </p>
-          </div>
-        </main>
-
-        <div className={styles.aboutSection}>
-          <div className={styles.aboutDescContainer}>
-            <p className={styles.aboutDescription}>
-              She specializes in botanical paintings and illustrations, monotone
-              portraits and custom handwritten calligraphy works on paper and
-              canvas. Looking beyond, she supports local artists and traditional
-              art.
-            </p>
-          </div>
-          <div className={styles.aboutDescContainer2}>
-            <p className={styles.aboutDescription}>
-              —— The artist believes that there is no greater gift than giving a
-              piece of art. Art is her manifestation of a timeless gift.
-            </p>
+    <div className={styles.preloadContainer}>
+      <div className={styles.container}>
+        <div className={styles.bgContainer}>
+          <img src="/overlay.jpeg" className={styles.bgOverlay} />
+          <div className={styles.bgContainer2}>
+            <img src="/overlay_2.jpeg" className={styles.bgOverlay2} />
           </div>
         </div>
+        <ReactBnbGallery
+          show={isOpen}
+          photos={PHOTOS}
+          activePhotoIndex={photoIndex}
+          onClose={() => setIsOpen(false)}
+        />
+        <Header />
+        <div className={styles.mainContainer}>
+          <Head>
+            <title>Inks and Watercolor</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-        <div className={styles.gallerySection} id="gallery">
-          <p className={styles.galleryTitle}>GALLERY</p>
-          <div className={styles.galleryContainer}>
-            {PHOTOS.map((image, index) => (
-              <div
-                className={styles.galleryObj}
-                onClick={() => {
-                  setIsOpen(true);
-                  setPhotoIndex(index);
-                }}
-              >
-                <img src={image} className={styles.galleryImg} />
-                <div className={styles.galleryImgText}>
-                  <p>Cinque Terre</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className={styles.contactSection}>
-          <a href="mailto:atienza.new@gmail.com">
-            <div className={styles.contact} id="contact">
-              CONTACT ME
+          <main className={styles.main}>
+            <div className={styles.landing}>
+              <p className={styles.landingTitle}>Inks & Watercolor</p>
             </div>
-          </a>
-          <i>
-            <p>Kara Diaz Serofia</p>
-          </i>
-        </div>
+            <div className={styles.descriptionContainer}>
+              <p className={styles.landingDescription}>
+                Inks and Watercolors is an incubator of modern art based in
+                Iloilo, Philippines. Stretching out of the comfort zone to
+                pursue painting and illustration in any medium.
+              </p>
+            </div>
+          </main>
 
-        <footer className={styles.footer}>
-          <p>
-            SITE designed and developed by :
-            <a href="https://www.mark-atienza.com" target="_blank">
-              Arvin Mark Atienza &copy; 2020
+          <div className={styles.aboutSection}>
+            <div className={styles.aboutDescContainer}>
+              <p className={styles.aboutDescription}>
+                She specializes in botanical paintings and illustrations,
+                monotone portraits and custom handwritten calligraphy works on
+                paper and canvas. Looking beyond, she supports local artists and
+                traditional art.
+              </p>
+            </div>
+            <div className={styles.aboutDescContainer2}>
+              <p className={styles.aboutDescription}>
+                —— The artist believes that there is no greater gift than giving
+                a piece of art. Art is her manifestation of a timeless gift.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.gallerySection} id="gallery">
+            <p className={styles.galleryTitle}>GALLERY</p>
+            <div className={styles.galleryContainer}>
+              {PHOTOS.map((image, index) => (
+                <div
+                  className={styles.galleryObj}
+                  onClick={() => {
+                    setIsOpen(true);
+                    setPhotoIndex(index);
+                  }}
+                >
+                  <img src={image} className={styles.galleryImg} />
+                  <div className={styles.galleryImgText}>
+                    <p>Cinque Terre</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className={styles.contactSection}>
+            <a href="mailto:atienza.new@gmail.com">
+              <div className={styles.contact} id="contact">
+                CONTACT ME
+              </div>
             </a>
-          </p>
-        </footer>
+            <i>
+              <p>Kara Diaz Serofia</p>
+            </i>
+          </div>
+
+          <footer className={styles.footer}>
+            <p>
+              SITE designed and developed by :
+              <a href="https://www.mark-atienza.com" target="_blank">
+                Arvin Mark Atienza &copy; 2020
+              </a>
+            </p>
+          </footer>
+        </div>
       </div>
     </div>
   );
