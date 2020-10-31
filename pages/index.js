488a87739a71ecc7274dac8bb6a731f5494efa33
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-10-31 10:32:42 +0800

    Added original images

--- pages/index.js
+++ pages/index.js
@@ -52,12 +52,12 @@ export default function Home() {
           <p className={styles.galleryTitle}>GALLERY</p>
           <div className={styles.galleryContainer}>
             {[
-              "overlay.jpeg",
-              "overlay_2.jpeg",
-              "overlay.jpeg",
-              "overlay_2.jpeg",
-              "overlay.jpeg",
-              "overlay_2.jpeg"
+              "gallery/1.jpeg",
+              "gallery/2.jpeg",
+              "gallery/3.jpeg",
+              "gallery/4.jpeg",
+              "gallery/5.jpeg",
+              "gallery/6.jpeg",
             ].map(image => (
               <div className={styles.galleryObj}>
                 <img src={image} className={styles.galleryImg} />
